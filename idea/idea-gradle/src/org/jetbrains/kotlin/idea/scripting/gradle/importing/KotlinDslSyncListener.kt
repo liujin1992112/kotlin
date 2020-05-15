41490079580b77a61dfa47a4aaff8a2904591d1e
Natalia Selezneva <natalia.selezneva@jetbrains.com>
2020-05-15 18:36:34 +0300

    fix order : todo 191

--- idea/idea-gradle/src/org/jetbrains/kotlin/idea/scripting/gradle/importing/KotlinDslSyncListener.kt
+++ idea/idea-gradle/src/org/jetbrains/kotlin/idea/scripting/gradle/importing/KotlinDslSyncListener.kt
@@ -8,26 +8,40 @@ package org.jetbrains.kotlin.idea.scripting.gradle.importing
 import com.intellij.openapi.externalSystem.model.task.ExternalSystemTaskId
 import com.intellij.openapi.externalSystem.model.task.ExternalSystemTaskNotificationListenerAdapter
 import com.intellij.openapi.externalSystem.model.task.ExternalSystemTaskType
+import org.jetbrains.kotlin.idea.core.script.ScriptDefinitionContributor
 import org.jetbrains.kotlin.idea.framework.GRADLE_SYSTEM_ID
+import org.jetbrains.kotlin.idea.scripting.gradle.GradleScriptDefinitionsContributor
 
 class KotlinDslSyncListener : ExternalSystemTaskNotificationListenerAdapter() {
     override fun onStart(id: ExternalSystemTaskId, workingDir: String?) {
         if (id.type != ExternalSystemTaskType.RESOLVE_PROJECT) return
         if (id.projectSystemId != GRADLE_SYSTEM_ID) return
 
-        if (workingDir == null) return
-        val project = id.findProject() ?: return
-
-        project.kotlinGradleDslSync[id] = KotlinDslGradleBuildSync(workingDir, id)
+        gradleState.isSyncInProgress = true
     }
 
     override fun onEnd(id: ExternalSystemTaskId) {
         if (id.type != ExternalSystemTaskType.RESOLVE_PROJECT) return
         if (id.projectSystemId != GRADLE_SYSTEM_ID) return
 
-        val project = id.findProject() ?: return
-        val sync = project.kotlinGradleDslSync.remove(id) ?: return
+        gradleState.isSyncInProgress = false
 
-        saveScriptModels(project, sync)
+        val project = id.findProject() ?: return
+
+        val gradleDefinitionsContributor =
+            ScriptDefinitionContributor.find<GradleScriptDefinitionsContributor>(
+                project
+            )
+        gradleDefinitionsContributor?.reloadIfNecessary()
     }
+
+    companion object {
+        internal val gradleState = GradleSyncState()
+    }
+}
+
+// TODO: state should be stored by gradle build,
+// now it is marked as complete after first gradle project was imported
+internal class GradleSyncState {
+    var isSyncInProgress: Boolean = false
 }
